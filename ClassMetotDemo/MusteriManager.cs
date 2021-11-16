43e15d95c3cb0096025c5cf97733d2dce801d27b
Cemalettin Altıntaş <93022253+cemalettinaltintas@users.noreply.github.com>
2021-11-16 10:18:29 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriler=new List<Musteri> ();
+        public MusteriManager()
+        {
+            
+        }
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" isimli müşteri eklendi!" );
+            musteriler.Add(musteri);
+        }
+        public List<Musteri> Listele()
+        {
+            return musteriler;
+            //foreach (var musteri in this.musteriler)
+            //{
+            //    Console.WriteLine("Müşteri Adı : " + musteri.Ad);
+            //    Console.WriteLine("Müşteri Soyadı : " + musteri.Soyad);
+            //    Console.WriteLine("Müşteri Maaşı : " + musteri.Maas);
+            //    Console.WriteLine("-------------------------------");
+            //}
+        }
+        public void Sil(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+            Console.WriteLine(musteri.Ad + " isimli müşteri silindi!");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Cemalettin";
+            musteri1.Soyad = "Altıntaş";
+            musteri1.Maas = 5000;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Oğuz";
+            musteri2.Soyad = "Altıntaş";
+            musteri2.Maas = 6000;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            foreach (var musteri in musteriManager.Listele())
+            {
+                Console.WriteLine(musteri.Ad);
+            }
+            
+            musteriManager.Sil(musteri1);
+        }
+
+    }
+}
